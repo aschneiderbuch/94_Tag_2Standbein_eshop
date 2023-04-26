f41534508f62e567d3ef6b6624c9c2d9bbc58618
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-04-26 15:47:59 +0200

    bE envdot + db Mongo

--- backEnd/util/db.js
+++ backEnd/util/db.js
@@ -1,3 +1,18 @@
+import { MongoClient } from 'mongodb'
 
+const URI = prozess.env.MONGO_URI
+const DB = prozess.env.MONGO_DB
 
-const 
+const client = new MongoClient(URI)
+
+let db 
+
+export const getDb = async () => {
+    if (db) {
+        return db
+    } else {
+        await client.connect()
+        db = client.db(DB)
+        return db
+    }
+}
